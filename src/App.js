f15fe4b402941bb9bfa62904e12cb012b0e42308
ravig1729 <ravigedela1729@gmail.com>
2022-02-22 11:32:52 +0530

    React Asgn 1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import LFuncCompo from './Assignment1/LFuncCompo'
+import LClassCompo from './Assignment1/LClassCompo'
+import './Assignment1/myStyle1.css'
+
+class App extends React.Component {
+  state = {
+    showClass: true,
+    showFunc: true
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="minus">Styling using Functional and Class Component</h1>
+        <button onClick = {() => this.setState({showFunc : !this.state.showFunc}) } className="first">To see styling in functional component</button>
+        <button onClick={() => this.setState({showClass : !this.state.showClass})} className="second">To see styling in class component</button>
+        {this.state.showFunc && <LFuncCompo /> ? null : <LFuncCompo />}
+        {this.state.showClass && <LClassCompo /> ? null : <LClassCompo />}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Assignment1/LFuncCompo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Inline1 from './Inline1'
+
+const LFuncCompo = () => {
+  return (
+    
+      <div className="box1">
+        <p className="third">This is created using functional Component</p>
+        <p className="fourth">This is done using external CSS </p>
+        < Inline1 />
+      </div>
+  )
+}
+
+export default LFuncCompo
